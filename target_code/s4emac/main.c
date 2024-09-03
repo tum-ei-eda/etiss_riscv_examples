110235b07de33390b3cb78e60a29527be443dced
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-09-03 10:27:45 +0200

    s4emac: update mnemonics to use s4e. prefix

--- target_code/s4emac/main.c
+++ target_code/s4emac/main.c
@@ -4,19 +4,19 @@ int main() {
   // Non-ISAX canary instruction, to flag an unwanted case of endian/width/compression/whatever
   asm("add x3, x4, x5");
 
-  asm("reset_acc");
+  asm("s4e.reset_acc");
 
-  asm("get_acc_lo x6");
+  asm("s4e.get_acc_lo x6");
 
-  asm("get_acc_hi x25");
+  asm("s4e.get_acc_hi x25");
 
-  asm("macu_32 x21, x17");
+  asm("s4e.macu_32 x21, x17");
 
-  asm("macs_32 x21, x17");
+  asm("s4e.macs_32 x21, x17");
 
-  asm("macu_64 x21, x17");
+  asm("s4e.macu_64 x21, x17");
 
-  asm("macs_64 x21, x17");
+  asm("s4e.macs_64 x21, x17");
 
   return 42;
 }
