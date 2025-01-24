9181d5229e988f33aa6d80bb8d26f5c42f0f4a03
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-01-24 15:44:03 +0100

    update mnn example for nmm.exth.b00,...

--- target_code/mnn/main.c
+++ target_code/mnn/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#nclude <stdio.h>
 #include <stdint.h>
 
 int main()
@@ -18,26 +18,113 @@ int main()
     // asm volatile(".insn i 0x2b, 0x7, %0, %1, %2" : "=r" (result) : "r" (x), "i" (increment) );
     // printf("Immediate Add result = %d\n", result);
 
-    uint32_t v8i4weights = 0x76543210;  // (+7 +6 +5 +4 +3 +2 +1 +0) -> 0b0111_0110_0101_0100_0011_0010_0001_0000
-    uint32_t v8i4weights2 = 0x89abcdef;  // (-8 -7 -6 -5 -4 -3 -2 -1) -> 0b1000_1001_1010_1011_1100_1101_1110_1111
+    uint32_t v4i8weights = 0x03020100;  // (+3 +2 +1 0) -> 0b00000011_00000010_00000001_00000000
+    uint32_t v4i8weights2 = 0x7f7e7d7c;  // (+127 +126 +125 +124) -> 0b01111111_01111110_01111101_01111100
+    uint32_t v4i8weights3 = 0xfcfdfeff;  // (-4 -3 -2 -1 | +252 +253 +254 +255) -> 0b11111100_11111101_11111110_11111111
+    uint32_t v4i8weights4 = 0x80818283;  // (-128 -127 -126 -125 | +128 +129 +130 +131) -> 0b10000000_10000001_10000010_10000011
     uint32_t unpacked = 0;
-    uint32_t unpacked2 = 0;
 
-    asm volatile("mnn.pair_extract %0, %1, 0" : "=r" (unpacked) : "r" (v8i4weights));
-    printf("unpacked[idx=0] = %08x\n", unpacked);    // expect 0x0001_0000
-    asm volatile("mnn.pair_extract %0, %1, 1" : "=r" (unpacked) : "r" (v8i4weights));
-    printf("unpacked[idx=1] = %08x\n", unpacked);    // expect 0x0003_0002
-    asm volatile("mnn.pair_extract %0, %1, 2" : "=r" (unpacked) : "r" (v8i4weights));
-    printf("unpacked[idx=2] = %08x\n", unpacked);    // expect 0x0005_0004
-    asm volatile("mnn.pair_extract %0, %1, 3" : "=r" (unpacked) : "r" (v8i4weights));
-    printf("unpacked[idx=3] = %08x\n", unpacked);    // expect 0x0007_0006
+    asm volatile("mnn.exths.b10 %0, %1" : "=r" (unpacked) : "r" (v4i8weights));
+    printf("unpacked[mnn.exths.b10] = %08x\n", unpacked);    // expect 0x0001_0000
+    asm volatile("mnn.exths.b20 %0, %1" : "=r" (unpacked) : "r" (v4i8weights));
+    printf("unpacked[mnn.exths.b20] = %08x\n", unpacked);    // expect 0x0002_0000
+    asm volatile("mnn.exths.b30 %0, %1" : "=r" (unpacked) : "r" (v4i8weights));
+    printf("unpacked[mnn.exths.b30] = %08x\n", unpacked);    // expect 0x0003_0000
+    asm volatile("mnn.exths.b21 %0, %1" : "=r" (unpacked) : "r" (v4i8weights));
+    printf("unpacked[mnn.exths.b21] = %08x\n", unpacked);    // expect 0x0002_0001
+    asm volatile("mnn.exths.b31 %0, %1" : "=r" (unpacked) : "r" (v4i8weights));
+    printf("unpacked[mnn.exths.b31] = %08x\n", unpacked);    // expect 0x0003_0001
+    asm volatile("mnn.exths.b32 %0, %1" : "=r" (unpacked) : "r" (v4i8weights));
+    printf("unpacked[mnn.exths.b32] = %08x\n", unpacked);    // expect 0x0003_0002
 
-    asm volatile("mnn.pair_extract %0, %1, 4" : "=r" (unpacked2) : "r" (v8i4weights2));
-    printf("unpacked2[idx=0] = %08x\n", unpacked2);    // expect 0xfffe_ffff
-    asm volatile("mnn.pair_extract %0, %1, 5" : "=r" (unpacked2) : "r" (v8i4weights2));
-    printf("unpacked2[idx=1] = %08x\n", unpacked2);    // expect 0xfffc_fffd
-    asm volatile("mnn.pair_extract %0, %1, 6" : "=r" (unpacked2) : "r" (v8i4weights2));
-    printf("unpacked2[idx=2] = %08x\n", unpacked2);    // expect 0xfffa_fffb
-    asm volatile("mnn.pair_extract %0, %1, 7" : "=r" (unpacked2) : "r" (v8i4weights2));
-    printf("unpacked2[idx=3] = %08x\n", unpacked2);    // expect 0xfff8_fff9
+    asm volatile("mnn.exths.b10 %0, %1" : "=r" (unpacked) : "r" (v4i8weights2));
+    printf("unpacked[mnn.exths.b10] = %08x\n", unpacked);    // expect 0x007d_007c
+    asm volatile("mnn.exths.b20 %0, %1" : "=r" (unpacked) : "r" (v4i8weights2));
+    printf("unpacked[mnn.exths.b20] = %08x\n", unpacked);    // expect 0x007e_007c
+    asm volatile("mnn.exths.b30 %0, %1" : "=r" (unpacked) : "r" (v4i8weights2));
+    printf("unpacked[mnn.exths.b30] = %08x\n", unpacked);    // expect 0x007f_007c
+    asm volatile("mnn.exths.b21 %0, %1" : "=r" (unpacked) : "r" (v4i8weights2));
+    printf("unpacked[mnn.exths.b21] = %08x\n", unpacked);    // expect 0x007e_007d
+    asm volatile("mnn.exths.b31 %0, %1" : "=r" (unpacked) : "r" (v4i8weights2));
+    printf("unpacked[mnn.exths.b31] = %08x\n", unpacked);    // expect 0x007f_007d
+    asm volatile("mnn.exths.b32 %0, %1" : "=r" (unpacked) : "r" (v4i8weights2));
+    printf("unpacked[mnn.exths.b32] = %08x\n", unpacked);    // expect 0x007f_007e
+
+    asm volatile("mnn.exths.b10 %0, %1" : "=r" (unpacked) : "r" (v4i8weights3));
+    printf("unpacked[mnn.exths.b10] = %08x\n", unpacked);    // expect 0xfffe_ffff
+    asm volatile("mnn.exths.b20 %0, %1" : "=r" (unpacked) : "r" (v4i8weights3));
+    printf("unpacked[mnn.exths.b20] = %08x\n", unpacked);    // expect 0xfffd_ffff
+    asm volatile("mnn.exths.b30 %0, %1" : "=r" (unpacked) : "r" (v4i8weights3));
+    printf("unpacked[mnn.exths.b30] = %08x\n", unpacked);    // expect 0xfffc_ffff
+    asm volatile("mnn.exths.b21 %0, %1" : "=r" (unpacked) : "r" (v4i8weights3));
+    printf("unpacked[mnn.exths.b21] = %08x\n", unpacked);    // expect 0xfffd_fffe
+    asm volatile("mnn.exths.b31 %0, %1" : "=r" (unpacked) : "r" (v4i8weights3));
+    printf("unpacked[mnn.exths.b31] = %08x\n", unpacked);    // expect 0xfffc_fffe
+    asm volatile("mnn.exths.b32 %0, %1" : "=r" (unpacked) : "r" (v4i8weights3));
+    printf("unpacked[mnn.exths.b32] = %08x\n", unpacked);    // expect 0xfffc_fffd
+
+    asm volatile("mnn.exths.b10 %0, %1" : "=r" (unpacked) : "r" (v4i8weights4));
+    printf("unpacked[mnn.exths.b10] = %08x\n", unpacked);    // expect 0xff82_ff83
+    asm volatile("mnn.exths.b20 %0, %1" : "=r" (unpacked) : "r" (v4i8weights4));
+    printf("unpacked[mnn.exths.b20] = %08x\n", unpacked);    // expect 0xff81_ff83
+    asm volatile("mnn.exths.b30 %0, %1" : "=r" (unpacked) : "r" (v4i8weights4));
+    printf("unpacked[mnn.exths.b30] = %08x\n", unpacked);    // expect 0xff80_ff83
+    asm volatile("mnn.exths.b21 %0, %1" : "=r" (unpacked) : "r" (v4i8weights4));
+    printf("unpacked[mnn.exths.b21] = %08x\n", unpacked);    // expect 0xff81_ff82
+    asm volatile("mnn.exths.b31 %0, %1" : "=r" (unpacked) : "r" (v4i8weights4));
+    printf("unpacked[mnn.exths.b31] = %08x\n", unpacked);    // expect 0xff80_ff82
+    asm volatile("mnn.exths.b32 %0, %1" : "=r" (unpacked) : "r" (v4i8weights4));
+    printf("unpacked[mnn.exths.b32] = %08x\n", unpacked);    // expect 0xff80_ff81
+
+    asm volatile("mnn.exthz.b10 %0, %1" : "=r" (unpacked) : "r" (v4i8weights));
+    printf("unpacked[mnn.exthz.b10] = %08x\n", unpacked);    // expect 0x0001_0000
+    asm volatile("mnn.exthz.b20 %0, %1" : "=r" (unpacked) : "r" (v4i8weights));
+    printf("unpacked[mnn.exthz.b20] = %08x\n", unpacked);    // expect 0x0002_0000
+    asm volatile("mnn.exthz.b30 %0, %1" : "=r" (unpacked) : "r" (v4i8weights));
+    printf("unpacked[mnn.exthz.b30] = %08x\n", unpacked);    // expect 0x0003_0000
+    asm volatile("mnn.exthz.b21 %0, %1" : "=r" (unpacked) : "r" (v4i8weights));
+    printf("unpacked[mnn.exthz.b21] = %08x\n", unpacked);    // expect 0x0002_0001
+    asm volatile("mnn.exthz.b31 %0, %1" : "=r" (unpacked) : "r" (v4i8weights));
+    printf("unpacked[mnn.exthz.b31] = %08x\n", unpacked);    // expect 0x0003_0001
+    asm volatile("mnn.exthz.b32 %0, %1" : "=r" (unpacked) : "r" (v4i8weights));
+    printf("unpacked[mnn.exthz.b32] = %08x\n", unpacked);    // expect 0x0003_0002
+
+    asm volatile("mnn.exthz.b10 %0, %1" : "=r" (unpacked) : "r" (v4i8weights2));
+    printf("unpacked[mnn.exthz.b10] = %08x\n", unpacked);    // expect 0x007d_007c
+    asm volatile("mnn.exthz.b20 %0, %1" : "=r" (unpacked) : "r" (v4i8weights2));
+    printf("unpacked[mnn.exthz.b20] = %08x\n", unpacked);    // expect 0x007e_007c
+    asm volatile("mnn.exthz.b30 %0, %1" : "=r" (unpacked) : "r" (v4i8weights2));
+    printf("unpacked[mnn.exthz.b30] = %08x\n", unpacked);    // expect 0x007f_007c
+    asm volatile("mnn.exthz.b21 %0, %1" : "=r" (unpacked) : "r" (v4i8weights2));
+    printf("unpacked[mnn.exthz.b21] = %08x\n", unpacked);    // expect 0x007e_007d
+    asm volatile("mnn.exthz.b31 %0, %1" : "=r" (unpacked) : "r" (v4i8weights2));
+    printf("unpacked[mnn.exthz.b31] = %08x\n", unpacked);    // expect 0x007f_007d
+    asm volatile("mnn.exthz.b32 %0, %1" : "=r" (unpacked) : "r" (v4i8weights2));
+    printf("unpacked[mnn.exthz.b32] = %08x\n", unpacked);    // expect 0x007f_007e
+
+    asm volatile("mnn.exthz.b10 %0, %1" : "=r" (unpacked) : "r" (v4i8weights3));
+    printf("unpacked[mnn.exthz.b10] = %08x\n", unpacked);    // expect 0x00fe_00ff
+    asm volatile("mnn.exthz.b20 %0, %1" : "=r" (unpacked) : "r" (v4i8weights3));
+    printf("unpacked[mnn.exthz.b20] = %08x\n", unpacked);    // expect 0x00fd_00ff
+    asm volatile("mnn.exthz.b30 %0, %1" : "=r" (unpacked) : "r" (v4i8weights3));
+    printf("unpacked[mnn.exthz.b30] = %08x\n", unpacked);    // expect 0x00fc_00ff
+    asm volatile("mnn.exthz.b21 %0, %1" : "=r" (unpacked) : "r" (v4i8weights3));
+    printf("unpacked[mnn.exthz.b21] = %08x\n", unpacked);    // expect 0x00fd_00fe
+    asm volatile("mnn.exthz.b31 %0, %1" : "=r" (unpacked) : "r" (v4i8weights3));
+    printf("unpacked[mnn.exthz.b31] = %08x\n", unpacked);    // expect 0x00fc_00fe
+    asm volatile("mnn.exthz.b32 %0, %1" : "=r" (unpacked) : "r" (v4i8weights3));
+    printf("unpacked[mnn.exthz.b32] = %08x\n", unpacked);    // expect 0x00fc_00fd
+
+    asm volatile("mnn.exthz.b10 %0, %1" : "=r" (unpacked) : "r" (v4i8weights4));
+    printf("unpacked[mnn.exthz.b10] = %08x\n", unpacked);    // expect 0x0082_0083
+    asm volatile("mnn.exthz.b20 %0, %1" : "=r" (unpacked) : "r" (v4i8weights4));
+    printf("unpacked[mnn.exthz.b20] = %08x\n", unpacked);    // expect 0x0081_0083
+    asm volatile("mnn.exthz.b30 %0, %1" : "=r" (unpacked) : "r" (v4i8weights4));
+    printf("unpacked[mnn.exthz.b30] = %08x\n", unpacked);    // expect 0x0080_0083
+    asm volatile("mnn.exthz.b21 %0, %1" : "=r" (unpacked) : "r" (v4i8weights4));
+    printf("unpacked[mnn.exthz.b21] = %08x\n", unpacked);    // expect 0x0081_0082
+    asm volatile("mnn.exthz.b31 %0, %1" : "=r" (unpacked) : "r" (v4i8weights4));
+    printf("unpacked[mnn.exthz.b31] = %08x\n", unpacked);    // expect 0x0080_0082
+    asm volatile("mnn.exthz.b32 %0, %1" : "=r" (unpacked) : "r" (v4i8weights4));
+    printf("unpacked[mnn.exthz.b32] = %08x\n", unpacked);    // expect 0x0080_0081
 }
