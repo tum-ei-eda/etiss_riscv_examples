292c8716a15167c3b20a4f019d0ac217d66d30e5
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-07-07 11:26:01 +0200

    share tvm data using symlink

--- target_code/tvm/vww_muriscvnn/vww_data/vww_model_settings.cc
+++
@@ -1,3 +0,0 @@
-#include "vww_model_settings.h"
-
-const char* vww_model_labels[] = {"person", "no person"};
--- target_code/tvm/vww_muriscvnn/vww_data/vww_output_data_ref.cc
+++
@@ -1,3 +0,0 @@
-#include "vww_output_data_ref.h"
-
-const uint8_t vww_output_data_ref[] = {0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0};
