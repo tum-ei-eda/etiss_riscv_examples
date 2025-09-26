ba7ff7a43627ca59eb8dc01198dc7f7f86eb4072
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-09-26 22:06:43 +0200

    fixes.c: add custom _exit syscall using SYS_EXIT_EXTENDED
    expose USE_SYS_EXIT_EXTENDED

--- riscv_crt0/fixes.c
+++ riscv_crt0/fixes.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdio.h>
 #include <sys/time.h>
 #include <sys/timeb.h>
 
@@ -16,3 +17,27 @@ int _gettimeofday(struct timeval *tv, void *_)
     tv->tv_usec = us % US_PER_S;
     return 0;
 }
+
+#ifdef USE_SYS_EXIT_EXTENDED
+#include <machine/syscall.h>
+#include "semihost_syscall.h"
+
+#define ADP_Stopped_ApplicationExit 0x20026
+
+/* Exit a program without cleaning up files.  */
+void
+_exit (int exit_status)
+{
+#if __riscv_xlen == 32
+  // syscall_errno (SEMIHOST_exit, (long *) ADP_Stopped_ApplicationExit);
+  long data_block[] = {ADP_Stopped_ApplicationExit, exit_status};
+  syscall_errno (SEMIHOST_exit_extended, data_block);
+#else
+  /* The semihosting exit operation only allows 64-bit targets to report the
+     exit code.  */
+  long data_block[] = {ADP_Stopped_ApplicationExit, exit_status};
+  syscall_errno (SEMIHOST_exit, data_block);
+#endif
+  while (1);
+}
+#endif  // USE_SYS_EXIT_EXTENDED
